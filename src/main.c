5b357df0a92c400b3311dcca239b1855e1590dc0
maklean <yoelmakaya@gmail.com>
2026-05-12 15:36:58 -0400

    refactor: remove redundant addConstant

--- src/main.c
+++ src/main.c
@@ -6,10 +6,8 @@ int main(int argc, const char **argv) {
     Chunk chunk;
 
     initChunk(&chunk);
-
-    int constant = addConstant(&chunk, 1.2);
-    writeChunk(&chunk, OP_CONSTANT, 123);
-    writeChunk(&chunk, constant, 123);
+    
+    writeConstant(&chunk, 1.2, 123);
 
     for(int i = 0; i < 256; i++) writeConstant(&chunk, i, 256);
 
