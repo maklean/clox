01669ce3f2e8273d2476ce7e13a858b8c9f5cdbf
maklean <yoelmakaya@gmail.com>
2026-06-14 12:33:28 -0400

    perf: use bitwise-AND to calculate modulo with table capacity (since it's always a power of two)

--- src/table.c
+++ src/table.c
@@ -80,7 +80,7 @@ void tableAddAll(Table *from, Table *to) {
 ObjString *tableFindString(Table *table, const char *chars, int length, uint32_t hash) {
     if(table->count == 0) return NULL;
 
-    uint32_t index = hash % table->capacity;
+    uint32_t index = hash & (table->capacity - 1);
     
     for(;;) {
         Entry *entry = &table->entries[index];
@@ -93,7 +93,7 @@ ObjString *tableFindString(Table *table, const char *chars, int length, uint32_t
             return entry->key;
         }
 
-        index = (index + 1) % table->capacity;
+        index = (index + 1) & (table->capacity - 1);
     }
 }
 
@@ -118,7 +118,7 @@ void markTable(Table *table) {
 }
 
 static Entry *findEntry(Entry *entries, int capacity, ObjString *key) {
-    uint32_t index = key->hash % capacity;
+    uint32_t index = key->hash & (capacity - 1);
     Entry *tombstone = NULL;
 
     for(;;) {
@@ -138,7 +138,7 @@ static Entry *findEntry(Entry *entries, int capacity, ObjString *key) {
         }
 
         // probe to another index
-        index = (index + 1) % capacity;
+        index = (index + 1) & (capacity - 1);
     }
 
     // unreachable
