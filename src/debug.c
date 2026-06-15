7f3850c95f3f7ee54a018c4500d009fd36136eec
maklean <yoelmakaya@gmail.com>
2026-06-14 20:54:23 -0400

    feat: add debugging for OP_MOD and OP_POW instructions (forgot to do this earlier...)

--- src/debug.c
+++ src/debug.c
@@ -53,6 +53,10 @@ int disassembleInstruction(Chunk *chunk, int offset) {
             return simpleInstruction("OP_SUBTRACT", offset);
         case OP_MULTIPLY:
             return simpleInstruction("OP_MULTIPLY", offset);
+        case OP_MOD:
+            return simpleInstruction("OP_MOD", offset);
+        case OP_POW:
+            return simpleInstruction("OP_POW", offset);
         case OP_DIVIDE:
             return simpleInstruction("OP_DIVIDE", offset);
         case OP_NEGATE:
